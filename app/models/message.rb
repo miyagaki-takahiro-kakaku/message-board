4edf6418bf50e1d8438f23db70dac3ae46811087
takahiro_miyagaki <miyagaki.takahiro.kakaku@gmail.com>
2016-04-22 07:44:31 +0000

    add age input

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
     validates :name, length: { maximum: 20 }, presence: true
+    validates :age,  numericality: { only_integer:true, greater_than_or_equal_to: 0 }
     validates :body, length: { minmum: 2, maximum: 30 }, presence: true
 end
