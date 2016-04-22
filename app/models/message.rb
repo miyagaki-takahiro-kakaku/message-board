45a0c87c84792343ea86d256d0886f83c488d537
takahiro_miyagaki <miyagaki.takahiro.kakaku@gmail.com>
2016-04-22 08:18:14 +0000

    add age validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ActiveRecord::Base
     validates :name, length: { maximum: 20 }, presence: true
-    validates :age,  numericality: { only_integer:true, greater_than_or_equal_to: 0 }
+  validates :age, numericality: { only_integer: true , greater_than_or_equal_to: 0}
+  def age=(value)
+    value.tr!('０-９', '0-9') if value.is_a?(String)
+    super(value)
+  end
     validates :body, length: { minmum: 2, maximum: 30 }, presence: true
 end
